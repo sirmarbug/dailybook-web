7ab909ed84c56186edd779bdf5a34a0d0bd225a1
Mariusz Bugajski <mbugajski.dev@gmail.com>
2020-01-11 11:06:29 +0100

    DB-10: Changed index.ts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import * as fromComponents from './components';
 
 const declarations = [
+  ...fromComponents.components
 ];
 
 const imports = [
