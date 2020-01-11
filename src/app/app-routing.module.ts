10f5fd129bb001c805bbc36a713b0bad1086cf41
Mariusz Bugajski <mbugajski.dev@gmail.com>
2020-01-11 12:41:55 +0100

    DB-1: Created auth routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./views/auth/auth.module').then(m => m.AuthModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
