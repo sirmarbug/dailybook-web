ce25b8414637be4fc3147abbe7d5da58bcc1bd67
Mariusz Bugajski <mbugajski.dev@gmail.com>
2020-01-12 13:54:56 +0100

    DB-11: Created security settings

--- src/app/views/dashboard/settings/account/account.component.ts
+++ src/app/views/dashboard/settings/account/account.component.ts
@@ -12,4 +12,7 @@ export class AccountComponent implements OnInit {
   ngOnInit() {
   }
 
+  onSaveClick(): void {
+  }
+
 }
--- src/app/views/dashboard/settings/security/security.component.ts
+++ src/app/views/dashboard/settings/security/security.component.ts
@@ -12,4 +12,7 @@ export class SecurityComponent implements OnInit {
   ngOnInit() {
   }
 
+  onSaveClick(): void {
+  }
+
 }
