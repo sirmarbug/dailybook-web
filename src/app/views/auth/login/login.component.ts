d18fcf4a99c676fd1afe45ffededa810eaa8c2d6
Mariusz Bugajski <mbugajski.dev@gmail.com>
2020-04-13 10:54:27 +0200

    Changed login component

--- src/app/views/auth/login/login.component.ts
+++ src/app/views/auth/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NGXLogger } from 'ngx-logger';
+import { auth } from 'firebase';
+import { SessionService } from '@core/services';
 
 @Component({
   selector: 'app-login',
@@ -8,15 +11,25 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
+  mail: string;
+  password: string;
+
   constructor(
-    private router: Router
+    private logger: NGXLogger,
+    private router: Router,
+    private sessionService: SessionService
   ) { }
 
   ngOnInit() {
   }
 
   onSignInClick(): void {
-    this.router.navigateByUrl('/dashboard/home');
+    this.logger.debug(this.mail);
+    // this.sessionService.loginWithEmail(this.mail, this.password)
+    //   .subscribe((res: auth.UserCredential) => {
+    //     this.logger.debug(res);
+    //     // this.router.navigateByUrl('/dashboard/home');
+    //   });
   }
 
 }
